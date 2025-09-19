272218e9a35b74942a841ec963ae63f13ba774f6
LENOVO-PC <zulkifliagus372@gmail.com>
2025-09-19 15:17:19 +0700

    initial commit

---
+++ src/main/kotlin/StringFungsiAnggota.kt
@@ -0,0 +1,29 @@
+package org.example
+
+import java.util.*
+
+fun main(args: Array<String>) {
+    val s = "Rhinopotamus"
+    println(s.startsWith("rhin"))
+    println(s.endsWith("tamus"))
+    println(s.contains("pot"))
+    println(s.contains("lol"))
+
+    var config = "Fullscreen shaDows autosave"
+    config = config.lowercase(Locale.getDefault())
+    println("Will the game run in fullscreen?")
+    println(config.contains("fullscreen"))
+    println("Will shadows be turned on?")
+    println(config.contains("shadows"))
+    println("Will sound be turned off?")
+    println(config.contains("nosound"))
+    println("Would the player like to use autosave?")
+    println(config.contains("autosave"))
+
+    var strJava = "Java is the best!"
+    strJava = strJava.replace("Java", "Kotlin")
+    println(s)
+    println("$strJava is ${strJava.length} characters long.")
+    println("I would not banish all of these Internets.".substring(2, 7))
+    println("alpha".compareTo("bravo"))
+}
--- src/main/kotlin/StringTemplates.kt
+++ src/main/kotlin/StringTemplates.kt
@@ -1,5 +1,14 @@
 package org.example
 
 fun main(args: Array<String>) {
-
+    val a = 7
+    val b = 8
+    val c = a + b
+    val s = "When we add $a and $b, we get $c"
+    println(s)
+    println("When we add $a and $b, we get ${a + b}")
+    val price = """
+        ${'$'}9.99
+    """
+    println(price)
 }
